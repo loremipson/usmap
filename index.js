87702501237729ba76072dedff84f5775829b7db
Tyler Ipson <tyler.ipson@clearlink.com>
2017-10-19 13:45:56 -0600

    Passing a "tooltips" object..

--- index.js
+++ index.js
@@ -2,7 +2,8 @@ import Raphael from 'raphael';
 import mapData from './mapData';
 import assign from 'object-assign-deep';
 
-export default (element, options) => {
+export default (element, options, tooltips = {}) => {
+
   const config = assign({
     'fill': '#d3d3d3',
     'stroke': '#fff',
@@ -15,6 +16,8 @@ export default (element, options) => {
     },
   }, options);
 
+  const allMapData = assign(mapData, tooltips);
+
   const R = Raphael(element); // eslint-disable-line
   const attr = config;
 
@@ -22,16 +25,18 @@ export default (element, options) => {
   R.setSize('100%', '100%');
 
   const handleClick = (url, state) => {
+    let newUrl = '';
+
     if (url.indexOf('{name}') > -1) {
       const friendlyState = state.name.replace(' ', '-').toLowerCase();
-      url = url.replace('{name}', friendlyState);
+      newUrl = url.replace('{name}', friendlyState);
     }
 
     if (url.indexOf('{abbr}') > -1) {
-      url = url.replace('{abbr}', state.abbr.text.toLowerCase());
+      newUrl = url.replace('{abbr}', state.abbr.text.toLowerCase());
     }
 
-    window.location = url;
+    window.location = newUrl;
   };
 
   const handleHover = (set, conf) => {
@@ -48,55 +53,59 @@ export default (element, options) => {
     });
   };
 
-  for (const state in mapData) {
+  for (const state in allMapData) {
+    if ({}.hasOwnProperty.call(allMapData, state)) {
+      console.log(state);
+      const set = R.set();
+      let shape = '';
 
-    const set = R.set();
-    let shape = '';
+      if (attr.labels && allMapData[state].hasOwnProperty('pathWithLabel')) {
+        shape = R.path(allMapData[state].pathWithLabel);
+      } else {
+        shape = R.path(allMapData[state].path);
+      }
 
-    if (attr.labels && mapData[state].hasOwnProperty('pathWithLabel')) {
-      shape = R.path(mapData[state].pathWithLabel);
-    } else {
-      shape = R.path(mapData[state].path);
-    }
+      set.push(shape.attr(attr));
 
-    set.push(shape.attr(attr));
+      if (attr.labels) {
+        const label = R.text(allMapData[state].abbr.posX, allMapData[state].abbr.posY, allMapData[state].abbr.text)
+          .attr(attr.label);
 
-    if (attr.labels) {
-      const label = R.text(mapData[state].abbr.posX, mapData[state].abbr.posY, mapData[state].abbr.text)
-        .attr(attr.label);
+        set.push(label);
+      }
 
-      set.push(label);
-    }
+      if (attr['link-template']) {
+        set.attr({ cursor: 'pointer' });
+      }
 
-    if (attr['link-template']) {
-      set.attr({ 'cursor': 'pointer' });
-    }
+      if (attr.hasOwnProperty('link-template')) {
+        set.click(() => {
+          handleClick(attr['link-template'], allMapData[state]);
+        });
+      }
 
-    if (attr.hasOwnProperty('link-template')) {
-      set.click(() => {
-        handleClick(attr['link-template'], mapData[state]);
-      });
-    }
+      if (attr.hasOwnProperty('hover-fill')) {
+        handleHover(set, attr);
+      }
 
-    if (attr.hasOwnProperty('hover-fill')) {
-      handleHover(set, attr);
-    }
+      if (attr.groups) {
+        for (const obj in attr.groups) {
+          if ({}.hasOwnProperty.call(attr.groups, obj)) {
+            const group = attr.groups[obj];
+            if (group.states.indexOf(allMapData[state].abbr.text) > -1) {
+              set.items[0].attr({ fill: group.fill });
 
-    if (attr.groups) {
-      for (const obj in attr.groups) {
-        const group = attr.groups[obj];
-        if (group.states.indexOf(mapData[state].abbr.text) > -1) {
-          set.items[0].attr({ fill: group.fill });
+              if (group['link-template']) {
+                set.attr({ cursor: 'pointer' });
+                set.click(() => {
+                  handleClick(group['link-template'], allMapData[state]);
+                });
+              }
 
-          if (group['link-template']) {
-            set.attr({ 'cursor': 'pointer' });
-            set.click(() => {
-              handleClick(group['link-template'], mapData[state]);
-            });
-          }
-
-          if (group['hover-fill']) {
-            handleHover(set, group);
+              if (group['hover-fill']) {
+                handleHover(set, group);
+              }
+            }
           }
         }
       }
